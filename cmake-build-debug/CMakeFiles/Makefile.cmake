65179234fb8c8aa4caba5529ff91500cea9b96c6
Mole <gospodinaristokrat@gmail.com>
2024-04-22 15:33:27 +0300

    commit -m "addCopyDbCMake"

--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -49,4 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/dbProgram.dir/DependInfo.cmake"
+  "CMakeFiles/CopyFile.dir/DependInfo.cmake"
   )
